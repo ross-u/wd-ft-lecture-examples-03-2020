c6bea499e77c07a26c758bd826f53f799cb060f0
ross-u <cirkovic.uros.srb@gmail.com>
2020-03-30 14:38:59 +0200

    Update w1 d1

---
+++ m1/w1/d1/04-js-intro/scripts/index.js
@@ -0,0 +1,93 @@
+console.log("Hello");
+
+// One line comment
+
+/* 
+Multi
+line 
+comment
+*/
+
+// Primtives
+var myString = "Uros"; // String
+var myNumber = 123; //  Number
+var myBoolean = true; // Boolean
+var myUndefined = undefined; // undefined - variable is declared but left empty on purpose
+var myNull = null; // null - variable is declared and initialized an empty value
+
+// Objects
+var myArray = [];
+var myObject = {};
+
+// Less common data types
+var mySymbol = Symbol();
+var myBigInt = 20938402938402n;
+
+var user; // variable declaration  (undefined)
+var age, email, address; // multiple variables declared - on one line
+
+user = "Alex"; // variable initialization
+age = 25; // variable initialization
+
+var user2 = "Sarah"; // declare and intialize
+var city = "Barcelona";
+
+// Naming a variable:
+// - JS is case sensitive
+// - First character can't be number
+var a;
+var color;
+var _private; // convention - common practice
+var $button;
+var camelCase; // camelCase
+var a_large_name; // kebab_case
+var thisWayIsCalledCamelCase;
+var π;
+
+var color = "red";
+var Color = "green";
+
+// reserved keywords
+
+// var var = 123;
+
+// var return = 123;
+
+var favoriteFood = "Steak";
+console.log(favoriteFood);
+console.log(typeof favoriteFood);
+
+favoriteFood = 567;
+console.log(favoriteFood);
+console.log(typeof favoriteFood);
+
+var favoriteFood = "Soup";
+// console.log(favoriteFood);
+
+var myVar = 100;
+var myUndefined = undefined;
+
+console.log(typeof myVar);
+console.log(typeof myUndefined);
+
+// const let   - create block scoped variables
+
+// const username2 = "Rapha";
+// username2 = "Capu";
+
+// const username3; // forces us to intialize the variable
+
+let favoriteGenre = "Rock";
+favoriteGenre = "Pop";
+
+// var maxAge = 60;
+// var minAge = 18;
+// var result = 2 + 2;
+
+// var mxa = 50;
+// var mia = 10;
+
+// var a = 'String';
+// var c = true;
+
+// var r = 2 + 2;
