cda4e06dd1412d17826d3055d0e244453637dbf3
ross-u <cirkovic.uros.srb@gmail.com>
2020-04-03 15:54:06 +0200

    Update w1 d5 classes example

---
+++ m1/w1/d5/01-oop-class/scripts/index.js
@@ -0,0 +1,104 @@
+// ES6
+
+// CREATING THE CLASS
+class Car {
+  constructor(brandParam, modelParam) {
+    // this = {}
+    this.brand = brandParam;
+    this.model = modelParam;
+    // this.printName = function () {
+    //   console.log(this.model + ' ' + this.brand);
+    // }
+  }
+
+  // After constructor block we can create methods that will be shared with all instances
+    drive () {
+      console.log(this.brand + ' drive around.');
+    }
+
+}
+
+// Car.prototype  - this is an empty object, automatically created on every class
+console.log('Car.prototype :', Car.prototype);
+
+// new  - used to instantiate new objects with `class` or the constructor
+const bmw = new Car('BMW', 'X5');
+const lexus = new Car('Lexus', 'L300');
+
+console.log('bmw :', bmw);
+console.log('lexus :', lexus);
+
+// Every object has a __proto__ property which points to its class prototype
+console.log(Car.prototype === bmw.__proto__);
+
+
+
+
+// EXTENDING THE CLASS
+class HybridCar extends Car {
+  constructor(brandParam, modelParam, engineParam) {
+    super(brandParam, modelParam); // call Car `class` to create `brand` and `model`
+    this.engine = engineParam;
+  }
+}
+
+
+const hybridToyota = new HybridCar('Toyota', 'Prius', 'electric engine');
+
+console.log('hybridToyota :', hybridToyota);
+
+
+
+
+
+// class Animal {
+//   constructor(name, legs, isAmphibian, isFlying) {
+//     this.name =name,
+//     this.legs =legs,
+//     this.isAmphibian =isAmphibian,
+//     this.isFlying = isFlying
+//   }
+
+//   walk () {}
+
+//   talk() {}
+// }
+
+
+// class Mammal extends Animal {
+//   constructor(name, legs, isAmphibian, isFlying, species, livesOnLand, a1, a2, a3) {
+//     super(name, legs, isAmphibian, isFlying);
+//     this.species,
+//     this.livesOnLand,
+//     this.a1,
+//     this.a2,
+//     this.a3
+//   }
+
+//   walksStanding() {}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cars = [
+  ['BMW', 'x6'],
+  ['toyota', 'hilux'],
+  ['seat', 'ateca']
+]
+
+// const newCars = cars.map( (carArr)=>  new Car(carArr[0], carArr[1] ));
+// console.log('newCars :', newCars);
